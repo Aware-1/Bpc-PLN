6eb24a7f928c7a9cd62566eef5e1d4fec50af8c5
Ali Eslami <99257606+Aware-1@users.noreply.github.com>
2025-07-14 11:24:07 +0330

    lot of 3

--- BPC-PLN/Domain/Dtos/HeaderBranchDto.cs
+++ BPC-PLN/Domain/Dtos/HeaderBranchDto.cs
@@ -2,5 +2,12 @@
 {
     public class HeaderBranchDto
     {
+        public string CLIENTREF { get; set; }
+        public DateTime Date { get; set; }
+        public string RequestCode { get; set; }
+        public string Address { get; set; }
+        public string DeliveryPlace { get; set; }
+        public string Definition { get; set; }
     }
+
 }
